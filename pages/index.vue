474300fe2d3bb8faf732faddd4586df4de11f179
Clayton VonDras <clayton@vondr.as>
2018-12-06 14:33:45 -0600

    It's not much, but it's something.

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,78 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        vondr.as
-      </h1>
-      <h2 class="subtitle">
-        personal site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <main>
+    <b-container fluid="">
+      <section class="cover py-5 px-3">
+        <div>
+          <h1 class="name .text-align-sm-center .text-align-lg-right">
+            Clayton<br>VonDras
+          </h1>
+        </div>
+      </section>
+    </b-container>
+    <div
+      class="text-white"
+      style="background-color: #222;">
+      <b-container class="py-5 bt-3">
+        <section
+          id="intro">
+          <!-- eslint-disable -->
+          <vue-markdown>
+# Welcome to my site.
+
+> It's incomplete, but it might be ready soon. Just you [wait](https://en.wikipedia.org/wiki/Heat_death_of_the_universe)!
+
+          </vue-markdown>
+        </section>
+      </b-container>
     </div>
-  </section>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import VueMarkdown from 'vue-markdown'
 
 export default {
   components: {
-    Logo
+    VueMarkdown
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Fira+Mono:400,500,700|IBM+Plex+Mono:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i|Roboto+Mono:100,100i,300,300i,400,400i,500,500i,700,700i&subset=greek,latin-ext');
+/* @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Mono:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i|Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto+Mono:100,100i,300,300i,400,400i,500,500i,700,700i&subset=greek,latin-ext'); */
 
-.container {
+* {
+  font-family: 'IBM Plex Mono', 'Gill Sans', 'Gill Sans MT', Calibri,
+    'Trebuchet MS', sans-serif;
+}
+
+h1,
+h2,
+h3 {
+  text-transform: uppercase;
+  font-weight: lighter;
+}
+
+.name {
+  font-weight: bolder;
+  font-family: 'Roboto Mono', monospace;
+  line-height: 0.75;
+  text-transform: uppercase;
+}
+.text-blue {
+  color: midnightblue;
+}
+.cover {
+  background-image: url('/images/constellation-clayton.png');
+  background-size: contain;
+  background-position: 0 bottom;
+  background-origin: 0;
+  background-repeat: no-repeat;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
